5589cf70c68181356d94342ede5f2477cc251ad7
HomyeeKing <HomyeeKing@gmail.com>
2022-06-01 23:34:19 +0800

    feat: add open panel

--- popup.js
+++ popup.js
@@ -1,97 +1,117 @@
-import store from "./store.js";
-import { rmQuery, updateQuery, showTips } from "./utils.js";
-import { reloadPage } from './chrUtils.js'
+import store from './store.js';
+import { rmQuery, updateQuery, showTips, addQuery } from './utils.js';
+import { reloadPage } from './chrUtils.js';
 
-const tbody = document.querySelector("#tbody");
-const template = document.querySelector("#productrow");
-const noData = document.querySelector("#nodata");
-const currentUrlNode = document.querySelector("#current-url");
+const tbody = document.querySelector('#tbody');
+const template = document.querySelector('#productrow');
+const noData = document.querySelector('#nodata');
+const currentUrlNode = document.querySelector('#current-url');
+// ald
+const configName = document.querySelector('#first-config-name');
+const subConfigName = document.querySelector('#subconfig-name');
+const resId = document.querySelector('#resId-input');
+const openPegasusPanel = document.querySelector('#open-pegasus-panel-btn');
 
-const warnText = document.querySelector(".warn-text");
+const warnText = document.querySelector('.warn-text');
 /**
  * 创建行
  * @param {String} v1 key的值
  * @param {String} v2 value的值
  */
-function createTr(v1 = "", v2 = "") {
-	let oldKey = v1;
+function createTr(v1 = '', v2 = '') {
+  let oldKey = v1;
 
-	const clone = template.content.cloneNode(true);
-	const tr = clone.querySelector("tr");
-	//   const checkbox = clone.querySelector(".checkbox");
+  const clone = template.content.cloneNode(true);
+  const tr = clone.querySelector('tr');
+  //   const checkbox = clone.querySelector(".checkbox");
 
-	const addBtn = clone.querySelector("#add-btn");
-	const removeBtn = clone.querySelector("#remove-btn");
-	const keyInput = clone.querySelector("#key-input");
-	const valueInput = clone.querySelector("#value-input");
-	keyInput.value = v1;
-	valueInput.value = v2;
+  const addBtn = clone.querySelector('#add-btn');
+  const removeBtn = clone.querySelector('#remove-btn');
+  const keyInput = clone.querySelector('#key-input');
+  const valueInput = clone.querySelector('#value-input');
+  keyInput.value = v1;
+  valueInput.value = v2;
 
-	//   function toggleCheckbox() {
-	//     console.log("click");
-	//     if (checkbox.checked && v1 !== "") {
-	//       store.url = addQuery({ [v1]: v2 });
-	//     } else {
-	//       store.url = rmQuery(v1);
-	//     }
-	//   }
-	//   创建行
-	addBtn.addEventListener("click", () => {
-		createTr();
-	});
+  //   function toggleCheckbox() {
+  //     console.log("click");
+  //     if (checkbox.checked && v1 !== "") {
+  //       store.url = addQuery({ [v1]: v2 });
+  //     } else {
+  //       store.url = rmQuery(v1);
+  //     }
+  //   }
+  //   创建行
+  addBtn.addEventListener('click', () => {
+    createTr();
+  });
 
-	//   移除行
-	removeBtn.addEventListener("click", () => {
-		removeTr(tr);
-		store.url = rmQuery(oldKey);
-	});
+  //   移除行
+  removeBtn.addEventListener('click', () => {
+    removeTr(tr);
+    store.url = rmQuery(oldKey);
+  });
 
-	keyInput.onchange = () => {
-		updateQuery(oldKey, { [keyInput.value]: valueInput.value });
-		oldKey = keyInput.value;
-	};
+  keyInput.onchange = () => {
+    updateQuery({ [keyInput.value]: valueInput.value });
+    oldKey = keyInput.value;
+  };
 
-	valueInput.onchange = () => {
-		updateQuery(oldKey, { [keyInput.value]: valueInput.value });
-	};
+  valueInput.onchange = () => {
+    updateQuery({ [keyInput.value]: valueInput.value });
+  };
 
-	//   checkbox.addEventListener("change", throttle(toggleCheckbox));
-	tbody.appendChild(clone);
+  //   checkbox.addEventListener("change", throttle(toggleCheckbox));
+  tbody.appendChild(clone);
 }
 
 function removeTr(node) {
-	if (tbody.childNodes.length === 1) {
-		tbody.removeChild(node);
-
-	}
+  if (tbody.childNodes.length === 1) {
+    tbody.removeChild(node);
+  }
 }
 
 chrome.runtime.sendMessage(
-	{
-		type: "getUrl",
-	},
-	(url) => {
-		// init url
-		store.pageUrl = url;
-		store.url = url;
-		currentUrlNode.textContent = store.url;
+  {
+    type: 'getUrl',
+  },
+  (url) => {
+    // init url
+    store.pageUrl = url;
+    store.url = url;
+    currentUrlNode.textContent = store.url;
 
-		const index = url.indexOf("?");
-		const query = index > -1 ? url.slice(index) : "";
-		console.log(`query`, query)
-		if (query === "") {
-			//   const clone = noData.content.cloneNode(true);
-			//   tbody.append(clone);
-			createTr('url参数为空')
-		} else {
-			const qs = new URLSearchParams(query);
-			for (const q of qs) {
-				createTr(q[0], q[1]);
-			}
-		}
-	}
+    const index = url.indexOf('?');
+    const query = index > -1 ? url.slice(index) : '';
+    if (query === '') {
+      //   const clone = noData.content.cloneNode(true);
+      //   tbody.append(clone);
+      createTr('url参数为空');
+    } else {
+      const qs = new URLSearchParams(query);
+      for (const q of qs) {
+        createTr(q[0], q[1]);
+      }
+    }
+  }
 );
 warnText.onclick = () => {
-	reloadPage(store.url)
-	showTips(false)
+  reloadPage(store.url);
+  showTips(false);
+};
+
+// open first ald
+openPegasusPanel.onclick = () => {
+  // 21046331
+  //
+  if (resId.value !== '') {
+    const newUrl = updateQuery({
+      activeResourceId: resId.value,
+      activeResourcePitId: resId.value,
+      aladdin: true,
+      tagInfo: subConfigName.value
+        ? `${configName.value}[hash=${resId.value}]/${subConfigName.value}`
+        : configName.value,
+    });
+    reloadPage(newUrl);
+  }
 };
--- utils.js
+++ utils.js
@@ -1,57 +1,76 @@
-import store from "./store.js";
+import store from './store.js';
 
+/**
+ *
+ * @param {} str
+ * @returns
+ */
 const queryToObj = (str) => {
-	const obj = Object.create(null);
-	if (!str || str === "") return obj;
+  const obj = Object.create(null);
+  if (!str || str === '') return obj;
 
-	const params = new URLSearchParams(str);
-	for (const [key, value] of params) {
-		obj[key] = value;
-	}
-	return obj;
+  const params = new URLSearchParams(str);
+  for (const [key, value] of params) {
+    obj[key] = value;
+  }
+  return obj;
 };
 
 const objToQuery = (obj) => {
-	const tmp = [];
-	for (const key of Object.keys(obj)) {
-		tmp.push(`${key}=${obj[key]}`);
-	}
-	return tmp.join("&");
+  const tmp = [];
+  for (const key of Object.keys(obj)) {
+    tmp.push(`${key}=${obj[key]}`);
+  }
+  return tmp.join('&');
+};
+
+/**
+ * return host,queryobj of url
+ * @param {String} url page url
+ * @returns
+ */
+const getPartFromUrl = (url = store.url) => {
+  if (url === '') return;
+  const [host, query] = url.split('?');
+  return { host, queryObj: queryToObj(query) };
 };
 
 export const rmQuery = (key, url = store.url) => {
-	if (key === "") return;
-	const [host, query] = url.split("?");
-	const queryObj = queryToObj(query);
-	Reflect.deleteProperty(queryObj, key);
-	return `${host}?${objToQuery(queryObj)}`;
+  if (key === '') return;
+  const { host, queryObj } = getPartFromUrl(url);
+  Reflect.deleteProperty(queryObj, key);
+  return `${host}?${objToQuery(queryObj)}`;
 };
 
 export const addQuery = (obj, url = store.url) => {
-	const [host, query] = url.split("?");
-	const queryObj = queryToObj(query);
-	Object.assign(queryObj, obj);
-	return `${host}?${objToQuery(queryObj)}`;
+  const { host, queryObj } = getPartFromUrl(url);
+  Object.assign(queryObj, obj);
+  return `${host}?${objToQuery(queryObj)}`;
 };
 
-export const updateQuery = (oldKey, newObj, url = store.url) => {
-	if (oldKey === "") return;
-	store.url = addQuery(newObj, rmQuery(oldKey, url));
+/**
+ * 更新页面query 没有就新增
+ * @param {object} newObj 要新增的键值对
+ * @param {String} url 当前页面链接
+ */
+export const updateQuery = (newObj, url = store.url) => {
+  const { queryObj: oldQuery } = getPartFromUrl(url);
+  store.url = addQuery({ ...oldQuery, ...newObj }, url);
+  return store.url;
 };
 
 export const throttle = (fn, timeout = 300) => {
-	let timer = undefined;
-	return () => {
-		if (timer) return;
-		timer = setTimeout(() => {
-			fn();
-			clearTimeout(timer);
-			timer = undefined;
-		}, timeout);
-	};
+  let timer = undefined;
+  return () => {
+    if (timer) return;
+    timer = setTimeout(() => {
+      fn();
+      clearTimeout(timer);
+      timer = undefined;
+    }, timeout);
+  };
 };
 
-
 export const showTips = (show = true) => {
-	document.querySelector(".warn-text").style.display = show ? 'inline' : 'none'
-}
+  document.querySelector('.warn-text').style.display = show ? 'inline' : 'none';
+};
